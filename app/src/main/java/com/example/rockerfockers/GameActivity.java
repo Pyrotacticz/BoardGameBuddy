533b1f41c65f96cccc7d7d621d0c4217319f4ebd
jatruong <jatruong@uw.edu>
2021-12-29 22:42:29 -0800

    Added Button to ActionBar.

--- app/src/main/java/com/example/rockerfockers/GameActivity.java
+++ app/src/main/java/com/example/rockerfockers/GameActivity.java
@@ -1,10 +1,13 @@
 package com.example.rockerfockers;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +25,9 @@ public class GameActivity extends AppCompatActivity {
         rvGame = (RecyclerView) findViewById(R.id.gameId);
 
         game = new Game();
-        getSupportActionBar().setTitle(game.getName());
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(game.getName());
 
         adapter = new GameAdapter(game);
 
@@ -31,8 +36,18 @@ public class GameActivity extends AppCompatActivity {
         rvGame.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
     public void addResource(View view) {
         game.resources.add(new Resource());
         adapter.notifyItemChanged(game.resources.size() - 1);
+    }
+
+    public void onEditAction(MenuItem mi) {
+        //TODO
     }
 }
